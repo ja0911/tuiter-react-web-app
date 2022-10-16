5691254e13959f2a08f874290735d98768b3c88a
Jennifer Adisoetjahya <ja0911@gmail.com>
2022-10-16 11:46:15 -0400

    a5 lab

---
+++ public/labs/a5/todos/todos.js
@@ -0,0 +1,13 @@
+export default [
+    {   title: 'Buy milk',
+        done: true,
+    },
+    {   title: 'Pickup the kids',
+        status: 'IN PROGRESS',
+        done: false,
+    },
+    {   title: 'Walk the dog',
+        done: false,
+    },
+ ];
+ 
